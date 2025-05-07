78b5e0ca28573c1938a0a7ae741d02d4b4f50900
XP-HS <3087953609@qq.com>
2025-05-07 18:04:00 +0800

    first commit

---
+++ multi_client_launcher.py
@@ -0,0 +1,67 @@
+import threading
+import socket
+import time
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+def client_task(name, file_path):
+    global message
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(('localhost', 54321))
+
+        if os.path.exists(file_path):
+            with open(file_path,'r') as file:
+                for line in file:
+                    line = line.strip()
+                    if line:
+                        parts =line.split()
+                        operation = parts[0]
+                        key = parts[1]
+                        
+                        if operation == "PUT":
+                            value = " ".join(parts[2:])
+                            message = str(len(key+value)+7).zfill(3)+" P "+key+" "+value
+                        elif operation == "READ":
+                            message = str(len(key)+6).zfill(3)+" R "+key
+                        elif operation == "GET":
+                            message = str(len(key)+6).zfill(3)+" G "+key
+                        # print(message)
+                        client_socket.sendall(message.encode('utf-8'))
+    
+        final = "STOP"
+        client_socket.sendall(final.encode('utf-8'))
+
+        response = client_socket.recv(999).decode('utf-8')
+        print(f"{name} received: {response}")
+    except Exception as e:
+        print(f"Error for {name}: {e}")
+    finally:
+        client_socket.close()
+        
+def main():
+    clients = []
+    file_paths = [
+        "test-workload\\client_1.txt",  
+        "test-workload\\client_2.txt",
+        "test-workload\\client_3.txt",
+        "test-workload\\client_4.txt",
+        "test-workload\\client_5.txt",
+        "test-workload\\client_6.txt",
+        "test-workload\\client_7.txt",
+        "test-workload\\client_8.txt",
+        "test-workload\\client_9.txt",
+        "test-workload\\client_10.txt",
+    ]
+    for i, file_path in enumerate(file_paths):
+        t = threading.Thread(target=client_task, args=(f'client-{i+1}', file_path))
+        clients.append(t)
+        t.start()
+        time.sleep(0.1)
+    
+    for t in clients:
+        t.join()
+
+if __name__ == "__main__":
+    main()
---
+++ multithread_server.py
@@ -0,0 +1,89 @@
+import socket
+import threading
+
+tuple_space = {}
+
+def read(key):
+    if key in tuple_space:
+        value = tuple_space[key]
+        return f"OK ({key}, {value}) read"
+    else:
+        return f"ERR {key} does not exist" 
+
+def get(key):
+    if key in tuple_space:
+        value = tuple_space.pop(key)
+        return f"OK ({key}, {value}) removed"
+    else:
+        return f"ERR {key} does not exist"
+
+def put(key, value):
+    if key in tuple_space:
+        return f"ERR {key} already exists"
+    else:
+        tuple_space[key] = value
+        return f"OK ({key}, {value}) added"
+
+def handle_client(client_socket, addr):
+    print(f"New client connected from {addr}")
+    
+    try:
+        while(True):
+            message = client_socket.recv(999).decode('utf-8')
+            #final = client_socket.recv(999).decode('utf-8')
+            # print(f"Client says: {message}")
+            if(message == "STOP"):
+                break  
+
+            parts = message.split()
+            operation = parts[1]
+            key = parts[2]
+
+            if operation == "R":
+                message2 = read(key)
+            elif operation == "G":
+                message2 = get(key)
+            elif operation == "P":
+                if len(parts) < 4:
+                    message2 = "ERR Invalid request"
+                else:
+                    value = " ".join(parts[3:])
+                    message2 = put(key, value)
+            else:
+                message2 = "ERR Invalid operation"
+            #print(message2)
+
+                  
+
+        message_length = str(len(message2)).zfill(3)
+        response = message_length + " " + message2
+        client_socket.sendall(response.encode('utf-8'))
+
+    except Exception as e:
+        print(f"Error in handling client {addr}: {e}")
+    finally:
+        client_socket.close()
+        print(f"Connection with {addr} has been closed.")
+
+def start_server():
+    host = 'localhost'
+    port = 54321
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_addr = (host, port)
+    server_socket.bind(sock_addr)
+    server_socket.listen(10)
+    print("Server is running and ready to accept multiple clients...")
+
+    try:
+        while True:
+            client_socket, addr = server_socket.accept()
+            client_handler = threading.Thread(target=handle_client, args=(client_socket, addr))
+            client_handler.start()
+    except KeyboardInterrupt:
+        print("Shutting down the server...")
+    finally:
+        server_socket.close()
+
+if __name__ == "__main__":
+    start_server()
